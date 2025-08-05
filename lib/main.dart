b5b3650f6ba8a54fc4edbb7cf926618650e04ff7
alokyadav686 <166922477+alokyadav686@users.noreply.github.com>
2025-08-05 13:51:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,5 @@ import 'package:my_medic/services/splash/splash_services.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AuthStorage.init();
-  
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Splashscreen()));
 }
